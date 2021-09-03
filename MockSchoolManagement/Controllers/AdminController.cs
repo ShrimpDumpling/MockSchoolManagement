678ae204868d7e3dc555fc62feefc7b8a71ace3f
jerry <anonyjie@outlook.com>
2021-09-03 19:58:45 +0800

    添加角色功能完成

--- MockSchoolManagement/Controllers/AdminController.cs
+++ MockSchoolManagement/Controllers/AdminController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using MockSchoolManagement.Models;
+using MockSchoolManagement.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,21 +10,52 @@ using System.Threading.Tasks;
 
 namespace MockSchoolManagement.Controllers
 {
-    [Authorize(Roles = "Admin")]
+
     public class AdminController : Controller
     {
         private readonly RoleManager<IdentityRole> _roleManager;
-        private readonly UserManager<ApplicationUser> _userManager;
+        
 
         public AdminController(
-            RoleManager<IdentityRole> roleManager,
-            UserManager<ApplicationUser> userManager)
+            RoleManager<IdentityRole> roleManager)
         {
             this._roleManager = roleManager;
-            this._userManager = userManager;
+            
         }
 
+        [HttpGet]
+        public IActionResult CreateRole()
+        {
+            return View();
+        }
 
+        [HttpPost]
+        public async Task<IActionResult> CreateRole(CreateRoleViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                IdentityRole identityRole = new IdentityRole
+                {
+                    Name = model.RoleName,
+                };
+                IdentityResult result = await _roleManager.CreateAsync(identityRole);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("index", "home");
+                }
+                foreach (IdentityError error in result.Errors)
+                {
+                    ModelState.AddModelError("", error.Description);
+                }
+            }
+            return View(model);
+        }
 
+        [HttpGet]
+        public IActionResult ListRoles()
+        {
+            var roles = _roleManager.Roles;
+            return View(roles);
+        }
     }
 }
---
+++ MockSchoolManagement/ViewModels/EditRoleViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MockSchoolManagement.ViewModels
+{
+    public class EditRoleViewModel
+    {
+        [Display(Name ="角色ID")]
+        public string Id { get; set; }
+
+        [Required(ErrorMessage ="角色名称是必填的")]
+        [Display(Name ="角色名称")]
+        public string RoleName { get; set; }
+
+        public List<string> Users { get; set; }
+    }
+}
